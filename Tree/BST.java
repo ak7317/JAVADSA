1af37f5ecd46d2fe443b5642c4e46fd5b0353d51
ak7317 <aamankhan31636@gmail.com>
2025-02-03 23:31:53 +0530

    validate bst

--- Tree/BST.java
+++ Tree/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BST {
     static class Node {
         int data;
@@ -98,6 +100,52 @@ public static Node findInorderSuccessor(Node root)
     }
     return root;
 }
+public static void printInRange(Node root , int k1 ,int k2)
+{
+    if(root == null)
+    {
+        return ;
+    }
+    if(root.data >= k1 && root.data <= k2 )
+    {
+        printInRange(root.left, k1, k2);
+        System.out.print(root.data+" ");
+        printInRange(root.right, k1, k2);
+    }
+    else if(root.data < k1)
+    {
+        printInRange(root.left, k1, k2);
+    }
+    else
+    {
+        printInRange(root.right, k1, k2);
+    }
+
+    
+}
+// helper of printroot 2 leaf
+public static void printPath( ArrayList<Integer> path)
+{
+    for(int i=0;i< path.size();i++){
+        System.out.print(path.get(i)+"->");
+
+    }
+    System.out.println("Null");
+}
+public static void printRoot2Leaf(Node root , ArrayList<Integer> path)
+{   if(root == null)
+    {
+        return ;
+    }
+    path.add(root.data);
+    if(root.left == null  && root.right == null)
+    {
+        printPath(path);
+    }
+    printRoot2Leaf(root.left, path);
+    printRoot2Leaf(root.right, path);
+    path.remove(path.size()-1);
+}
     
     
 public static void main(String args[]) {
@@ -118,8 +166,10 @@ public static void main(String args[]) {
     //     System.out.print("Not found");
     // }
     // System.out.print(search(root, 3));
-    delete(root, 1);
-    inorder(root);
+    // delete(root, 1);
+    // inorder(root);
+    // printInRange(root, 5, 14);
+    printRoot2Leaf(root, new ArrayList<>());
 
 
 }    
